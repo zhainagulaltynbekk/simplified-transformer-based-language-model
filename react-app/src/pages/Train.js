91d743288eeb827ae9a465c01bef16fd474fce0c
Zhainagul Altynbek Kyzy <zhainagul.altynbek.kyzy@ericsson.com>
2024-04-27 10:53:16 +0200

    Train page designed properly

--- react-app/src/pages/Train.js
+++ react-app/src/pages/Train.js
@@ -9,19 +9,19 @@ import { useState } from 'react';
 
 const Train = () => {  
   const [formData, setFormData] = useState({
-    batch_size: '',
-    block_size: '',
-    max_iters: '',
-    eval_interval: '',
-    learning_rate: '',
-    device: '',
-    eval_iters: '',
-    n_embd: '',
-    n_head: '',
-    n_layer: '',
-    dropout: '',
+    batch_size: '32', // Default value
+    block_size: '128', // Default value
+    max_iters: '10', // Default value
+    eval_interval: '100', // Default value
+    learning_rate: '3e-4', // Default value
+    device: 'cpu', // Default value
+    eval_iters: '1', // Default value
+    n_embd: '384', // Default value
+    n_head: '8', // Default value
+    n_layer: '8', // Default value
+    dropout: '0.2', // Default value
     file: null
-  });
+  });  
   
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -59,43 +59,61 @@ const Train = () => {
         <div className="main">
         <h2 className='pageName'>Train</h2>
         <form className='param-form' onSubmit={handleSubmit}>
-          <label>Batch Size</label>
-          <input className='form-item' type="number" name="batch_size" placeholder="Batch Size" value={formData.batch_size} onChange={handleChange} />
-
-          <label>Block Size</label>
-          <input className='form-item' type="number" name="block_size" placeholder="Block Size" value={formData.block_size} onChange={handleChange} />
-
-          <label>Max Iterations</label>
-          <input className='form-item' type="number" name="max_iters" placeholder="Max Iterations" value={formData.max_iters} onChange={handleChange} />
-
-          <label>Evaluation Interval</label>
-          <input className='form-item' type="number" name="eval_interval" placeholder="Evaluation Interval" value={formData.eval_interval} onChange={handleChange} />
-
-          <label>Learning Rate</label>
-          <input className='form-item' type="number" name="learning_rate" placeholder="Learning Rate" value={formData.learning_rate} onChange={handleChange} />
-
-          <label>Device</label>
-          <input className='form-item' type="text" name="device" placeholder="Device" value={formData.device} onChange={handleChange} />
-
-          <label>Evaluation Iterations</label>
-          <input className='form-item' type="number" name="eval_iters" placeholder="Evaluation Iterations" value={formData.eval_iters} onChange={handleChange} />
-
-          <label>Number of Embeddings</label>
-          <input className='form-item' type="number" name="n_embd" placeholder="Number of Embeddings" value={formData.n_embd} onChange={handleChange} />
-
-          <label>Number of Heads</label>
-          <input className='form-item' type="number" name="n_head" placeholder="Number of Heads" value={formData.n_head} onChange={handleChange} />
-
-          <label>Number of Layers</label>
-          <input className='form-item' type="number" name="n_layer" placeholder="Number of Layers" value={formData.n_layer} onChange={handleChange} />
-
-          <label>Dropout Rate</label>
-          <input className='form-item' type="number" name="dropout" placeholder="Dropout Rate" value={formData.dropout} onChange={handleChange} />
-
-          <label>Model File</label>
-          <input type="file" onChange={handleFileChange} />
-
-          <button type="submit">Submit</button>
+          <div className='form-row'>
+            <div className='form-item'>
+              <label className='label'>Batch Size</label>
+              <input className='form-input' type="number" name="batch_size" placeholder="Batch Size" value={formData.batch_size} onChange={handleChange} />
+            </div>
+            <div className='form-item'>
+              <label className='label'>Block Size</label>
+              <input className='form-input' type="number" name="block_size" placeholder="Block Size" value={formData.block_size} onChange={handleChange} />
+            </div>
+            <div className='form-item'>
+              <label className='label'>Max Iterations</label>
+              <input className='form-input' type="number" name="max_iters" placeholder="Max Iterations" value={formData.max_iters} onChange={handleChange} />
+            </div>
+            <div className='form-item'>
+              <label className='label'>Evaluation Interval</label>
+              <input className='form-input' type="number" name="eval_interval" placeholder="Evaluation Interval" value={formData.eval_interval} onChange={handleChange} />
+            </div>
+          </div>
+          <div className='form-row'>
+            <div className='form-item'>
+              <label className='label'>Learning Rate</label>
+              <input className='form-input' type="number" name="learning_rate" placeholder="Learning Rate" value={formData.learning_rate} onChange={handleChange} />
+            </div>
+            <div className='form-item'>
+              <label className='label'>Device</label>
+              <input className='form-input' type="text" name="device" placeholder="Device" value={formData.device} onChange={handleChange} />
+            </div>
+            <div className='form-item'>
+              <label className='label'>Evaluation Iterations</label>
+              <input className='form-input' type="number" name="eval_iters" placeholder="Evaluation Iterations" value={formData.eval_iters} onChange={handleChange} />
+            </div>
+            <div className='form-item'>
+              <label className='label'>Number of Embeddings</label>
+              <input className='form-input' type="number" name="n_embd" placeholder="Number of Embeddings" value={formData.n_embd} onChange={handleChange} />
+            </div>
+          </div>
+          <div className='form-row'>
+            <div className='form-item'>
+              <label className='label'>Number of Heads</label>
+              <input className='form-input' type="number" name="n_head" placeholder="Number of Heads" value={formData.n_head} onChange={handleChange} />
+            </div>
+            <div className='form-item'>
+              <label className='label'>Number of Layers</label>
+              <input className='form-input' type="number" name="n_layer" placeholder="Number of Layers" value={formData.n_layer} onChange={handleChange} />
+            </div>
+            <div className='form-item'>
+              <label className='label'>Dropout Rate</label>
+              <input className='form-input' type="number" name="dropout" placeholder="Dropout Rate" value={formData.dropout} onChange={handleChange} />
+            </div>
+            <div className='form-item'>
+              <label className='label'>Model File</label>
+              <input className='form-file' type="file" onChange={handleFileChange} />
+            </div>
+          </div>
+          <button type="submit" className='form-btn'>Submit</button>
         </form>
         </div>
       </div>
